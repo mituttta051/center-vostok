337c65ffe87473219fccf44ba23c09e4cd7a4350
mituttta051 <amityutneva@mail.ru>
2024-07-31 11:09:25 +0300

    feat: header and hero section added

---
+++ src/components/header.tsx
@@ -0,0 +1,30 @@
+'use client'
+import {FaEnvelope, FaPhone} from "react-icons/fa";
+import {HiMenu} from "react-icons/hi";
+interface HeaderProps {
+    toggleMenu: () => void;
+}
+export default function Header(props: HeaderProps) {
+
+    return (
+        <header className="bg-violet-50 text-default-violet-900 py-4 border-b border-b-violet-200">
+            <div className="w-full px-4 flex justify-between items-center">
+                <div className="flex items-center space-x-3">
+                    <HiMenu className="h-8 w-8 cursor-pointer" onClick={props.toggleMenu}/>
+                    <img src="/mainLogo.png" alt="Logo" className="h-10 mr-4"/>
+                    <span className="text-2xl font-bold">ТК Центр-Восток</span>
+                </div>
+                <div className="hidden md:flex space-x-8">
+                    <div className="flex items-center space-x-2">
+                        <FaPhone/>
+                        <span>+7 (123) 456-7890</span>
+                    </div>
+                    <div className="flex items-center space-x-2">
+                        <FaEnvelope/>
+                        <span>info@centr-vostok.ru</span>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
---
+++ src/components/hero.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default function Hero() {
+    return (
+        <div className="relative h-[calc(100vh-50px)] flex items-center justify-center text-center bg-gray-900">
+            <video
+                className="absolute top-0 left-0 w-full h-full object-cover opacity-50"
+                autoPlay
+                loop
+                muted
+            >
+                <source src="/Mountain%20Train.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+            <div className="relative z-10 p-4">
+                <h1 className="text-4xl md:text-6xl text-gray-200 font-bold">
+                    Перевозим ваш бизнес вперед
+                </h1>
+            </div>
+        </div>
+    );
+};
