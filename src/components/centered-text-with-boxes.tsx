da5a75daf28f5af10a23065f59a45cfd83043c76
mituttta051 <amityutneva@mail.ru>
2024-08-02 12:54:54 +0300

    feat: added icons to section text img, common styles for boxes added

--- src/components/centered-text-with-boxes.tsx
+++ src/components/centered-text-with-boxes.tsx
@@ -1,44 +1,43 @@
 import React from 'react';
 
-const CenteredTextWithBoxes: React.FC = () => {
+export default function CenteredTextWithBoxes() {
+    const commonStyles = "h-full shadow-md rounded-lg flex text-center items-center justify-center text-lg py-10 max-w-48 px-5"
     return (
-        <div className="relative min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="relative min-h-[800px] flex items-center justify-center bg-gray-100">
             <div
-                className="absolute grid grid-rows-3 grid-cols-3 gap-4 items-center justify-center w-full max-w-screen-lg p-10">
+                className="absolute grid grid-rows-3 grid-cols-3 gap-4 justify-items-center items-stretch justify-center w-full max-w-screen-xl p-10">
                 {/* Two boxes above the main text */}
                 <div className="grid grid-cols-5 col-span-3 row-start-1">
                     <div
-                        className="col-start-2 w-full h-full bg-white shadow-md rounded-lg flex items-center justify-center">
-                        <p>Транспортно-экспедиционные услуги</p>
+                        className={`text-default-darkBlue col-start-2 ${commonStyles}`}>
+                        <p className="break-words">Транспортно-экспедиционные услуги</p>
                     </div>
                     <div
-                        className="col-start-4 w-full h-full bg-white shadow-md rounded-lg flex items-center justify-center">
-                        <p>Организация перевозок грузов</p>
+                        className={`text-default-violet-50 col-start-4 ${commonStyles}`}>
+                        <p className="break-words ">Организация перевозок грузов</p>
                     </div>
                 </div>
                 {/* Two boxes on the same line as the main text */}
                 <div
-                    className="col-start-1 row-start-2 w-full h-full bg-white shadow-md rounded-lg flex items-center justify-center">
-                    <p>Диспетчеризация продвижения грузов</p>
+                    className={`text-default-violet-50 col-start-1 row-start-2 ${commonStyles}`}>
+                    <p className="break-words">Диспетчеризация продвижения грузов</p>
                 </div>
                 <div
-                    className="col-start-3 row-start-2 w-full h-full bg-white shadow-md rounded-lg flex items-center justify-center">
-                    <p>Организация рабочих мест с последующим карьерным ростом</p>
+                    className={`text-default-darkBlue col-start-3 row-start-2 ${commonStyles}`}>
+                    <p className="break-words">Организация рабочих мест с последующим карьерным ростом</p>
                 </div>
 
                 {/* Main Text */}
-                <div className="col-start-2 row-start-2 w-full h-full flex items-center justify-center">
-                    <h1 className="text-2xl font-bold">Main Text</h1>
+                <div className="col-start-2 row-start-2 h-full flex text-center items-center justify-center py-10 max-w-48 px-5">
+                    <h1 className="text-4xl font-bold">Центр Восток - это</h1>
                 </div>
 
                 {/* One box below the main text */}
                 <div
-                    className="col-start-2 row-start-3 w-full h-full bg-white shadow-md rounded-lg flex items-center justify-center">
-                    <p>Оплата ЖД тарифа</p>
+                    className={`text-default-violet-50 col-start-2 row-start-3 ${commonStyles}`}>
+                    <p className="break-words">Оплата ЖД тарифа</p>
                 </div>
             </div>
         </div>
     );
 };
-
-export default CenteredTextWithBoxes;
