a7ba2ae79e5231a2a9b3016fb5be6e3eaa0abda4
mituttta051 <amityutneva@mail.ru>
2024-08-04 10:27:57 +0300

    feat: added real images to section carousel and logos to icon section

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,11 +1,13 @@
 'use client'
 import React, { useState } from 'react';
+import SectionTitle from "@/components/common/section-title";
 
 export default function Carousel() {
     const images = [
-        'https://via.placeholder.com/600x400/FF0000/FFFFFF?text=Slide+1',
-        'https://via.placeholder.com/600x400/00FF00/FFFFFF?text=Slide+2',
-        'https://via.placeholder.com/600x400/0000FF/FFFFFF?text=Slide+3',
+        '/client-carousel/notebooks.png',
+        '/client-carousel/calendar.png',
+        '/client-carousel/cake1.png',
+        '/client-carousel/cake2.png'
     ];
 
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -23,40 +25,44 @@ export default function Carousel() {
     };
 
     return (
-        <div className="relative w-full max-w-2xl mx-auto overflow-hidden my-10">
-            <div className="flex transition-transform duration-500" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
-                {images.map((image, index) => (
-                    <img key={index} src={image} alt={`Slide ${index}`} className="w-full flex-shrink-0" />
-                ))}
-            </div>
+        <div className="p-10">
+            <SectionTitle title="Мы благодарим партнеров за сотрудничество" className="text-center" />
+            <div className="relative w-full max-w-2xl mx-auto my-10">
+                <div className="overflow-hidden">
+                    <div className="flex transition-transform duration-500"
+                         style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+                        {images.map((image, index) => (
+                            <img key={index} src={image} alt={`Slide ${index}`} className="w-full flex-shrink-0" />
+                        ))}
+                    </div>
+                </div>
 
-            <button
-                onClick={prevSlide}
-                className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-default-violet-50 text-white p-2 rounded-full"
-            >
-                &lt;
-            </button>
+                <button
+                    onClick={prevSlide}
+                    className="absolute left-2 -ml-10 top-1/2 transform -translate-y-1/2 bg-default-violet-50 text-white p-2 rounded-full"
+                >
+                    &lt;
+                </button>
 
-            <button
-                onClick={nextSlide}
-                className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-default-violet-50 text-white p-2 rounded-full"
-            >
-                &gt;
-            </button>
+                <button
+                    onClick={nextSlide}
+                    className="absolute right-2 -mr-10 top-1/2 transform -translate-y-1/2 bg-default-violet-50 text-white p-2 rounded-full"
+                >
+                    &gt;
+                </button>
 
-            <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 flex space-x-2 pb-4">
-                {images.map((_, index) => (
-                    <button
-                        key={index}
-                        onClick={() => goToSlide(index)}
-                        className={`w-3 h-3 rounded-full ${
-                            index === currentIndex ? 'bg-default-violet-900' : 'bg-default-violet-50'
-                        }`}
-                    />
-                ))}
+                <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 -mb-6 flex space-x-2">
+                    {images.map((_, index) => (
+                        <button
+                            key={index}
+                            onClick={() => goToSlide(index)}
+                            className={`w-3 h-3 rounded-full ${
+                                index === currentIndex ? 'bg-default-violet-900' : 'bg-default-violet-50'
+                            }`}
+                        />
+                    ))}
+                </div>
             </div>
         </div>
     );
 };
-
-
--- src/components/icon-section.tsx
+++ src/components/icon-section.tsx
@@ -1,43 +1,33 @@
 import React from 'react';
-import {
-    FaAnchor,
-    FaAndroid,
-    FaApple,
-    FaBeer,
-    FaBicycle,
-    FaBus,
-    FaCar,
-    FaCat,
-    FaCoffee,
-    FaDog,
-    FaFish,
-    FaPlane,
-    FaRocket,
-    FaShip,
-    FaSkull,
-    FaTrain
-} from 'react-icons/fa';
 import SectionTitle from "@/components/common/section-title";
 
 const icons = [
-    {icon: <FaBeer/>, label: 'Beer'},
-    {icon: <FaCoffee/>, label: 'Coffee'},
-    {icon: <FaApple/>, label: 'Apple'},
-    {icon: <FaAndroid/>, label: 'Android'},
-    {icon: <FaCat/>, label: 'Cat'},
-    {icon: <FaDog/>, label: 'Dog'},
-    {icon: <FaFish/>, label: 'Fish'},
-    {icon: <FaCar/>, label: 'Car'},
-    {icon: <FaBicycle/>, label: 'Bicycle'},
-    {icon: <FaBus/>, label: 'Bus'},
-    {icon: <FaPlane/>, label: 'Plane'},
-    {icon: <FaTrain/>, label: 'Train'},
-    {icon: <FaShip/>, label: 'Ship'},
-    {icon: <FaRocket/>, label: 'Rocket'},
-    {icon: <FaSkull/>, label: 'Skull'},
-    {icon: <FaAnchor/>, label: 'Anchor'},
-    {icon: <FaAnchor/>, label: 'Anchor'},
-    // Add more icons as needed
+    {icon: <img src="/logos/aemz-logo.svg" alt="AЭМЗ" />, label: 'АЭМЗ'},
+    {icon: <img src="/logos/belshpala.svg" alt="БелШпала" />, label: 'БелШпала'},
+    {icon: <img src="/logos/Carbon%20Group.png" alt="Carbon Group" />, label: 'Carbon Group'},
+    {icon: <img src="/logos/Gerkules%20logo.svg" alt="Gerkules" />, label: 'Gerkules'},
+    {icon: <img src="/logos/GK%20logo.png" alt="ТЭМПО" />, label: 'ТЭМПО'},
+    {icon: <img src="/logos/glogistic.png" alt="GLogistic" />, label: 'GLogistic'},
+    {icon: <img src="/logos/gok.png" alt="ГОК Ташлинский" />, label: 'ГОК Ташлинский'},
+    {icon: <img src="/logos/knauf.png" alt="knauf" />, label: 'knauf'},
+    {icon: <img src="/logos/kpz-novliansky.png" alt="КПЗ Новлянский" />, label: 'КПЗ Новлянский'},
+    {icon: <img src="/logos/mashstal.jpg" alt="МашСталь" />, label: 'МашСталь'},
+    {icon: <img src="/logos/ngbs.jpg" alt="Нефтегазбурсервис" />, label: 'Нефтегазбурсервис'},
+    {icon: <img src="/logos/ngok.png" alt="Новокаолиновый ГОК" />, label: 'Новокаолиновый ГОК'},
+    {icon: <img src="/logos/novokyznetsky-melkombinat.png" alt="Новокузнецкий мелькомбинат" />, label: 'Новокузнецкий мелькомбинат'},
+    {icon: <img src="/logos/novorosmetall.svg" alt="Новоросметалл" />, label: 'Новоросметалл'},
+    {icon: <img src="/logos/novorossiiskiy-prokatniy-zavod.svg" alt="Новороссийский прокатный завод" />, label: 'Новороссийский прокатный завод'},
+    {icon: <img src="/logos/Novostal%20logo.svg" alt="Новосталь-М" />, label: 'Новосталь-М'},
+    {icon: <img src="/logos/omega-trans.png" alt="Омега Транс" />, label: 'Омега Транс'},
+    {icon: <img src="/logos/russtal.png" alt="Руссталь" />, label: 'Руссталь'},
+    {icon: <img src="/logos/rvrz.png" alt="Рославльский ВРЗ" />, label: 'Рославльский ВРЗ'},
+    {icon: <img src="/logos/rzhd.png" alt="РЖД" />, label: 'РЖД'},
+    {icon: <img src="/logos/salavatsteklo.jpeg" alt="Салаватстекло" />, label: 'Салаватстекло'},
+    {icon: <img src="/logos/sopz.png" alt="СОПЗ" />, label: 'СОПЗ'},
+    {icon: <img src="/logos/soya.png" alt="ЗАО Соя" />, label: 'ЗАО Соя'},
+    {icon: <img src="/logos/stroysmes.png" alt="Завод Стройсмесь" />, label: 'Завод Стройсмесь'},
+    {icon: <img src="/logos/technonikol.svg" alt="Технониколь" />, label: 'Технониколь'},
+    {icon: <img src="/logos/tolmachevstiyzavod.png" alt="Толмачевский завод" />, label: 'Толмачевский завод'},
 ];
 
 export default function IconsSection() {
@@ -45,9 +35,9 @@ export default function IconsSection() {
         <>
             <div className="container mx-auto px-4 py-8">
                 <SectionTitle title={"Наши клиенты"} className={"ml-10 mb-10"}/>
-                <div className="grid grid-cols-8 gap-8">
+                <div className="grid md:grid-cols-9 grid-cols-3 gap-8">
                     {icons.map((item, index) => (
-                        <div key={index} className="flex flex-col items-center">
+                        <div key={index} className="flex flex-col items-center justify-center h-[180px]">
                             <div className="relative group">
                                 <div className="text-4xl">
                                     {item.icon}
