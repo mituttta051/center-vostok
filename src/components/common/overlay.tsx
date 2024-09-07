e62c20a0e42841ce915d90d2fe045b4ca027b8cf
mituttta051 <amityutneva@mail.ru>
2024-09-07 09:41:21 +0300

    fix: overlay above navbar

--- src/components/common/overlay.tsx
+++ src/components/common/overlay.tsx
@@ -22,7 +22,7 @@ const Overlay: React.FC<OverlayProps> = ({ isVisible, onClose }) => {
 
     return (
         <div
-            className="fixed inset-0 bg-black bg-opacity-50 z-30 flex items-center justify-center"
+            className="fixed inset-0 top-[72px] bg-black bg-opacity-50 z-30 flex items-center justify-center"
             onClick={onClose}
         >
 
